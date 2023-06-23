603cdbee25dc3d0b639f558963295045d53bdb18
Gnomedebian <yassineamgarou@gmail.com>
2023-06-22 17:04:22 -0700

    edit task 10

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,32 +1,24 @@
 #include "main.h"
 
 /**
- * print_triangle - prints a triangle, followed by a new line
- * @size: size of the triangle
+ * print_triangle - Prints a triangle using the character '#'
+ * @size: The size of the triangle
  */
 void print_triangle(int size)
 {
+	int row, col;
+
 	if (size <= 0)
 	{
 		_putchar('\n');
+		return;
 	}
-	else
+
+	for (row = 0; row < size; row++)
 	{
-		int i, j;
+		for (col = 0; col <= row; col++)
+			_putchar('#');
 
-		for (i = 1; i <= size; i++)
-		{
-			for (j = i; j < size; j++)
-			{
-				_putchar(' ');
-			}
-
-			for (j = 1; j <= i; j++)
-			{
-				_putchar('#');
-			}
-
-			_putchar('\n');
-		}
+		_putchar('\n');
 	}
 }
