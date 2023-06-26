4b8cff2db52912f5d74f685df0c99f339f562c38
Gnomedebian <yassineamgarou@gmail.com>
2023-06-26 16:46:36 -0700

    push tas 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * reset_to_98 -  Write a function
+ *
+ * @n: var assigned int
+ *
+ * Return: null
+ */
+void reset_to_98(int *n)
+{
+
+	*n = 98;
+
+}
