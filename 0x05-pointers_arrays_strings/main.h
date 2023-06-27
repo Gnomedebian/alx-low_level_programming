089a855fdf43f22434d8f61f51bd8d17c507f980
Gnomedebian <yassineamgarou@gmail.com>
2023-06-26 17:55:33 -0700

    edit

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -2,6 +2,7 @@
 #define MAIN_H
 
 /* Function prototypes */
+int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
@@ -12,6 +13,7 @@ void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
+
 
 
 #endif
