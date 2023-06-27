70746650025cb67f0cc3298780ab4a644e19a58e
Gnomedebian <yassineamgarou@gmail.com>
2023-06-26 17:33:40 -0700

    edit task 2

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,21 +1,17 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * _strlen - check the code
- * @s: The string to find the length of.
- *
- * Return: lenght
+ * _strlen - swaps the values
+ * @s: string parameter
+ * Return: lenght of string
  */
 int _strlen(char *s)
 {
-	int lenght = 0'
-	
-	while (*s != '\0')
-	{
-		lenght++;
-		s++;
-	}
+	int counter;
 
-	return lenght;
+	for (counter = 0; *s != '\0'; s++)
+	{
+		++counter;
+	}
+	return (counter);
 }
