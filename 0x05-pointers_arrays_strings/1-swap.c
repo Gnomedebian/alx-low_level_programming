7efa3503158d3fc0a43d75fd6851a8d1c18166b3
Gnomedebian <yassineamgarou@gmail.com>
2023-06-26 17:10:25 -0700

    push task 1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * swap_int - check the code
+ *
+ * @a : mmm
+ * @b: mmm
+ *
+ * Return: null
+ */
+void swap_int(int *a, int *b)
+{
+
+	int tmp = *a;
+	*a = *b;
+	*b = tmp;
+
+}
