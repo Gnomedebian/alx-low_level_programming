09f01778aa0975fdd71a3880ce7108e683ab6843
Gnomedebian <yassineamgarou@gmail.com>
2023-07-10 03:59:38 -0700

    edit 2

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,32 +1,24 @@
-i#include "main.h"
+#include "main.h"
 #include <stdlib.h>
-
 /**
- * create_array - create array of chars
- * @size: array size
- * @c: Char to initialize with
+ * create_array - create array of size size and assign char c
+ * @size: size of array
+ * @c: char to assign
+ * Description: create array of size size and assign char c
+ * Return: pointer to array, NULL if fail
  *
- * Return: Pointer to array
  */
 char *create_array(unsigned int size, char c)
 {
-	char *arr;
+	char *str;
 	unsigned int i;
 
-	i = 0;
-
-	if (size == 0)
+	str = malloc(sizeof(char) * size);
+	if (size == 0 || str == NULL)
 		return (NULL);
 
-	arr = malloc(sizeof(char) * size);
-
-	if (arr == NULL)
-		return (NULL);
-
-	while (i < size)
-	{
-		arr[i] = c;
-		i++;
-	}
-	return (arr);
+	for (i = 0; i < size; i++)
+		str[i] = c;
+	return (str);
 }
+
