bee31ad74bf1ec7dcc14c24ecf775a75f03de7f3
Gnomedebian <yassineamgarou@gmail.com>
2023-07-14 17:12:52 -0700

    push tasks

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,44 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * string_nconcat - concatenates two strings
+ * @s1: first string
+ * @s2: second string
+ * @n: the first n bytes of s2
+ * Return: pointer to the allocated memory
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+char *p;
+unsigned int i = 0, k = 0, len = 0, size;
+
+if (s1 == NULL)
+s1 = "";
+
+if (s2 == NULL)
+s2 = "";
+
+while (len < strlen(s1))
+len++;
+size = (len + n) * sizeof(*p);
+p = malloc(size + 1);
+if (p == NULL)
+return (NULL);
+
+while (i < size && i < strlen(s1))
+{
+p[i] = s1[i];
+i++;
+}
+
+while (i < size && k < strlen(s2))
+{
+p[i] = s2[k];
+i++;
+k++;
+}
+p[i] = '\0';
+return (p);
+}
