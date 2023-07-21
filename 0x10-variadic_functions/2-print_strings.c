8954b8e9be4ba0023e689790238bb3b408362b10
Gnomedebian <yassineamgarou@gmail.com>
2023-07-21 15:00:01 -0700

    push 2

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -5,21 +5,21 @@
 /**
  * print_strings - Prints strings, followed by a new line.
  * @separator: The string to be printed between strings.
- * @n: The number of strings passed to the function.
+ * @a: The number of strings passed to the function.
  * @...: A variable number of strings to be printed.
  *
  * Description: If separator is NULL, it is not printed.
  *              If one of the strings if NULL, (nil) is printed instead.
  */
-void print_strings(const char *separator, const unsigned int n, ...)
+void print_strings(const char *separator, const unsigned int a, ...)
 {
 	va_list strings;
 	char *str;
 	unsigned int index;
 
-	va_start(strings, n);
+	va_start(strings, a);
 
-	for (index = 0; index < n; index++)
+	for (index = 0; index < a; index++)
 	{
 		str = va_arg(strings, char *);
 
@@ -28,7 +28,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		else
 			printf("%s", str);
 
-		if (index != (n - 1) && separator != NULL)
+		if (index != (a - 1) && separator != NULL)
 			printf("%s", separator);
 	}
 
