321a72caa55b486166a3da2113384d4b50cb0e4f
Gnomedebian <yassineamgarou@gmail.com>
2023-08-07 22:36:35 +0100

    edit betty style

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -2,12 +2,13 @@
 #include <stdlib.h>
 
 /**
- * read_textfile - reads a text file and prints it to the POSIX standard output.
+ * read_textfile - reads a text file and prints
+ *	it to the POSIX standard output.
  * @filename: A pointer to name of file.
  * @letters: The number of letters it should read and print.
  *
  * Return: if filename is NULL, if the file can not be opened or read
- *         if write fails or does not write the expected amount of bytes 0.
+ *	if write fails or does not write the expected amount of bytes 0.
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
